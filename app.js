c17838229890fc6b17e0f0ba6d8093f011a01e4a
jhonatann-per <jhonatanpereira152@gmail.com>
2024-11-29 23:43:00 -0300

    back-end do gerenciador de estoque criado e configurado

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+app.use(express.json()); // para receber informações do insominia no formato JSON
+
+app.get("/", async (req, res)=> {
+    res.send("Bem vindo! ao projeto")
+});
+
+app.post("/cadastrar", async (req, res)=> {
+    console.log(req.body)
+    res.send("Produto Cadastrado com Sucesso")
+});
+
+app.listen(8080, () =>{
+    console.log("servidor iniciado na porta: http://localhost:8080")
+});
