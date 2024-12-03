f91310ba0e5110faa7816a71046387f492906a2e
jhonatann-per <jhonatanpereira152@gmail.com>
2024-12-03 00:56:26 -0300

    Implementação e teste das rotas de deleção e edição concluídos

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.post("/cadastrar", async (req, res) => {
         const novoProduto = await Produto.create({
             nome, preco_venda, quantidade
         });
-        res.status(201).json({
+        res.status(200).json({
             mensagem: "Produto cadastrado com sucesso!",
             produto: novoProduto
         });
@@ -43,12 +43,37 @@ app.get("/visualizar/:id", async (req, res) => {
         if (produto) {
             res.status(200).json(produto);
         } else {
-            res.status(404).json({ mensagem: "Produto não encontrado" });
+            res.status(400).json({ mensagem: "Produto não encontrado" });
         }
     } catch (error) {
-        res.status(400).json({ mensagem: "Erro: Não foi possível visualizar o produto" });
+        res.status(405).json({ mensagem: "Erro: Não foi possível visualizar o produto" });
     }
 });
+
+app.put("/editar-produto", async (req, res) => {
+    const { id } = req.body;
+
+    const [updated] = await Produto.update(req.body, { where: { id } });
+    if (updated) {
+        return res.status(200).json({ erro: false, mensagem: "Produto editado com sucesso!" });
+    } else {
+        return res.status(404).json({ erro: true, mensagem: "Erro: Produto não encontrado ou não editado" });
+    }
+});
+
+
+app.delete("/deletar-produto/:id", async (req, res) => {
+    const { id } = req.params;
+
+    const resultado = await Produto.destroy({ where: { id } });
+
+    if (resultado) {
+        return res.status(200).json({ erro: false, mensagem: "Produto apagado com sucesso!" });
+    } else {
+        return res.status(404).json({ erro: true, mensagem: "Erro: Produto não encontrado ou não apagado" });
+    }
+});
+
 
 
 app.listen(8080, () =>{
